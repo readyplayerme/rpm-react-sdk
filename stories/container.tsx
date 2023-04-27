9da02869774c065744c87872358520cd9cbb8238
Sercan Altundas <srcnalt@users.noreply.github.com>
2023-04-27 23:47:44 +0300

    Create container.tsx

---
+++ stories/container.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { FC } from "react"
+
+interface ContainerProps {
+    children: React.ReactNode
+}
+
+export const Container : FC<ContainerProps> = ({children}: ContainerProps) => {
+    return <div style={{height: 700, width: 700, marginLeft: 'auto', marginRight: 'auto', borderRadius: 8, overflow: 'hidden'}}>{children}</div>
+}
