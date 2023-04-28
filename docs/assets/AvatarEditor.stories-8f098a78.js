ed035f83eb79bf608319e927e72b020c265be6a9
srcnalt <srcnalt@users.noreply.github.com>
2023-04-28 14:59:33 +0000

    Deploying to gh-pages from @ readyplayerme/rpm-react-sdk@8aac4cd16fbe39ad12e47826757aa6281c1bda07 🚀

---
+++ docs/assets/AvatarEditor.stories-8f098a78.js
@@ -0,0 +1,40 @@
+import{A as a,a as r,C as d}from"./container-871468c0.js";import"./index-ebeaab24.js";const y={title:"Avatar Editor",component:a},n={render:e=>r(d,{children:r(a,{...e})}),args:{subdomain:"demo",editorConfig:{clearCache:!0,language:"en"}}},o={render:e=>r(d,{children:r(a,{...e})}),args:{subdomain:"demo",editorConfig:{bodyType:"halfbody",clearCache:!0,language:"en"}}},t={render:e=>r(d,{children:r(a,{...e})}),args:{subdomain:"demo",editorConfig:{quickStart:!0,clearCache:!0,language:"en"}}},s={render:e=>r(d,{children:r(a,{...e})}),args:{subdomain:"demo",editorConfig:{language:"de",clearCache:!0}}};var i,c,g;n.parameters={...n.parameters,docs:{...(i=n.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: (args: AvatarEditorProps) => <Container><AvatarEditor {...args} /></Container>,
+  args: {
+    subdomain: 'demo',
+    editorConfig: {
+      clearCache: true,
+      language: 'en'
+    }
+  }
+}`,...(g=(c=n.parameters)==null?void 0:c.docs)==null?void 0:g.source}}};var u,m,l;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: (args: AvatarEditorProps) => <Container><AvatarEditor {...args} /></Container>,
+  args: {
+    subdomain: 'demo',
+    editorConfig: {
+      bodyType: 'halfbody',
+      clearCache: true,
+      language: 'en'
+    }
+  }
+}`,...(l=(m=o.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};var p,C,f;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: (args: AvatarEditorProps) => <Container><AvatarEditor {...args} /></Container>,
+  args: {
+    subdomain: 'demo',
+    editorConfig: {
+      quickStart: true,
+      clearCache: true,
+      language: 'en'
+    }
+  }
+}`,...(f=(C=t.parameters)==null?void 0:C.docs)==null?void 0:f.source}}};var b,h,A;s.parameters={...s.parameters,docs:{...(b=s.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: (args: AvatarEditorProps) => <Container><AvatarEditor {...args} /></Container>,
+  args: {
+    subdomain: 'demo',
+    editorConfig: {
+      language: 'de',
+      clearCache: true
+    }
+  }
+}`,...(A=(h=s.parameters)==null?void 0:h.docs)==null?void 0:A.source}}};const S=["Default","Halfbody","QuickStart","LanguageGerman"];export{n as Default,o as Halfbody,s as LanguageGerman,t as QuickStart,S as __namedExportsOrder,y as default};
+//# sourceMappingURL=AvatarEditor.stories-8f098a78.js.map
