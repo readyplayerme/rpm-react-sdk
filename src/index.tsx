a032bbc4e0570e016900beade8d5fbb51422fafe
Sercan Altundas <srcnalt@users.noreply.github.com>
2023-05-04 18:06:55 +0300

    fix: import path fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 export { AvatarCreator as AvatarCreator } from './components/avatar-creator';
-export { AvatarCreatorViewer } from './components/AvatarCreatorViewer';
+export { AvatarCreatorViewer } from './components/avatar-creator-viewer';
 export type { EditorConfig, ViewerConfig, AvatarConfig, Language, BodyType } from './types';
 export type { EventName, TextureChannel, MorphTargets } from './types';
